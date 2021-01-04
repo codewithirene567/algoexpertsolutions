0ded9f0f1a90c995c548229ed9050e346fadb3dd
codewithirene567 <irenejinscott@gmail.com>
2021-01-03 23:50:49 -0500

    added an if condition

--- binarySearch.js
+++ binarySearch.js
@@ -3,3 +3,5 @@ function binarySearch(array, target) {
     let right = array.length-1
     for (let i=0; i<array.length; i++){
         let middle = Math.floor((left+right) / 2)
+        if(array[middle] === target){
+            return array.indexOf(target)
