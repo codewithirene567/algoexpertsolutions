d8a4f062232bbcaad9215c7a790be38b3e80a074
codewithirene567 <irenejinscott@gmail.com>
2021-01-03 23:50:14 -0500

    added a for loop to my solution

--- binarySearch.js
+++ binarySearch.js
@@ -1,4 +1,5 @@
 function binarySearch(array, target) {
     let left = 0
     let right = array.length-1
-    
+    for (let i=0; i<array.length; i++){
+        let middle = Math.floor((left+right) / 2)
